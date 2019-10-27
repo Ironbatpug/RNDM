de1226f5cc6488430559822c036b239a6b17b561
Ironbatpug <molnarcsaba92@gmail.com>
2019-10-27 12:40:28 +0100

    initial commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Molnár Csaba on 2019. 10. 27..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
