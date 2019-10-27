e283a82b4adbf3ec5fd5b3075b01972106ad8957
Ironbatpug <molnarcsaba92@gmail.com>
2019-10-27 18:20:29 +0100

    lesson7 login page add

---
+++ RNDM/Controller/CreateUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateUserViewController.swift
+//  RNDM
+//
+//  Created by Molnár Csaba on 2019. 10. 27..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class CreateUserViewController: UIViewController {
+    @IBOutlet weak var emailtxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var createUserBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func createUserBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func canceBtnPressed(_ sender: Any) {
+    }
+    
+}
---
+++ RNDM/Controller/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  RNDM
+//
+//  Created by Molnár Csaba on 2019. 10. 27..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var createUserBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func loginBtnTapped(_ sender: Any) {
+    }
+    
+
+    @IBAction func createUserTapped(_ sender: Any) {
+    }
+    
+}
